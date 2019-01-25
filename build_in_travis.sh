eaf875c09628aebd3c3e12eb60f939b152ff2658
Rajendra Kumar <rjdkmr@gmail.com>
2019-01-25 16:06:53 +0100

    update travis config

--- build_in_travis.sh
+++ build_in_travis.sh
@@ -4,27 +4,6 @@ set -e -x
 mkdir external
 cd external
 
-# Install slightly newer version of cmake 
-curl -O https://cmake.org/files/v3.0/cmake-3.0.2.tar.gz
-tar -zxvf cmake-3.0.2.tar.gz
-cd cmake-3.0.2
-./bootstrap
-make -j2
-make install
-cd ..
-
-# Install eigen3
-curl -L -O http://bitbucket.org/eigen/eigen/get/3.2.10.tar.gz
-mv 3.2.10.tar.gz eigen-3.2.10.tar.gz
-mkdir eigen-3.2.10
-tar --directory=eigen-3.2.10 --strip-components=1 -zxvf eigen-3.2.10.tar.gz
-cd eigen-3.2.10
-mkdir build
-cd build
-cmake ..
-make -j2 && make install
-cd ..
-cd ..
 
 # Install GROMACS
 curl -L -O http://ftp.gromacs.org/pub/gromacs/gromacs-2016.5.tar.gz
