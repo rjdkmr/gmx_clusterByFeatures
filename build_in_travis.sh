b488f40f9b3b429169fad43641ee24c9acdfb3cf
Rajendra Kumar <rjdkmr@gmail.com>
2019-01-28 12:22:23 +0100

    Update build_in_travis.sh

--- build_in_travis.sh
+++ build_in_travis.sh
@@ -27,7 +27,7 @@ cd build
 if [[ $1 != 'osx' ]]; then
   cmake .. -DGMX_GPU=off -DBUILD_SHARED_LIBS=ON -DGMX_PREFER_STATIC_LIBS=ON -DCMAKE_CXX_FLAGS="-static-libstdc++" -DGMX_SIMD=SSE2
 else
-  cmake .. -DGMX_GPU=off -DBUILD_SHARED_LIBS=ON -DGMX_PREFER_STATIC_LIBS=ON -DGMX_SIMD=SSE2 -DGMX_GSL=ON \
+  cmake .. -DGMX_GPU=off -DGMX_SIMD=SSE2 -DGMX_GSL=ON \
            -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" \
            -DOpenMP_CXX_LIB_NAMES="omp" -DOpenMP_omp_LIBRARY=$(brew --prefix libomp)/lib/libomp.a \
            -DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" \
