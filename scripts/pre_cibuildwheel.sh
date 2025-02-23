c2de0793613c4bdb062a8a6c3e8347d9d0241481
Rajendra Kumar <8026549+rjdkmr@users.noreply.github.com>
2025-02-23 20:08:29 +0530

    Update pre_cibuildwheel.sh

--- scripts/pre_cibuildwheel.sh
+++ scripts/pre_cibuildwheel.sh
@@ -6,6 +6,8 @@ cd external
 mkdir gmx_installed 
 cd gromacs
 
+ls -lrta
+
 if [ -d build ]; then
     rm -rf build
 fi
@@ -16,10 +18,7 @@ cd build
 export GMX_INSTALL=${CWD}/external/gmx_installed
 export GMX_SRC=${CWD}/external/gromacs
 
-echo $CWD
-ls -lrta
-
-cmake .. -DGMX_SIMD=SSE2 -DGMX_GPU=off -DGMXAPI=OFF -DGMX_INSTALL_LEGACY_API=on -DGMX_FFT_LIBRARY=fftpack -DCMAKE_INSTALL_PREFIX=${GMX_INSTALL}
+cmake -DGMX_SIMD=SSE2 -DGMX_GPU=off -DGMXAPI=OFF -DGMX_INSTALL_LEGACY_API=on -DGMX_FFT_LIBRARY=fftpack -DCMAKE_INSTALL_PREFIX=${GMX_INSTALL} ..
 make
 make install
 
