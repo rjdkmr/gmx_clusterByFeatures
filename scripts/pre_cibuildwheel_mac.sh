ac4431248dcbb14ceefa30e796517ea5180b631a
Rajendra Kumar <rjdkmr@gmail.com>
2025-02-24 10:50:00 +0000

    mac actions init

---
+++ scripts/pre_cibuildwheel_mac.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e -x
+
+CWD=`pwd`
+cd external
+mkdir gmx_installed 
+cd gromacs
+
+ls -lrta
+
+if [ -d build ]; then
+    rm -rf build
+fi
+
+mkdir build
+cd build
+
+export GMX_INSTALL=${CWD}/external/gmx_installed
+export GMX_SRC=${CWD}/external/gromacs
+
+cmake -DGMX_SIMD=SSE2 -DGMX_GPU=off -DGMXAPI=OFF -DGMX_INSTALL_LEGACY_API=on -DGMX_FFT_LIBRARY=fftpack -DCMAKE_INSTALL_PREFIX=${GMX_INSTALL} ..
+make
+make install
+
+cd $CWD
