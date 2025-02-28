8061f65e61788fa872aa664bbf3c595a4ebbc625
Rajendra Kumar <rjdkmr@gmail.com>
2025-02-28 17:45:45 +0000

    fixed actions file

--- scripts/pre_cibuildwheel.sh
+++ scripts/pre_cibuildwheel.sh
@@ -18,7 +18,7 @@ cd build
 export GMX_INSTALL=${CWD}/external/gmx_installed
 export GMX_SRC=${CWD}/external/gromacs
 
-cmake -DGMX_SIMD=SSE2 -DGMX_GPU=off -DGMXAPI=OFF -DGMX_INSTALL_LEGACY_API=on -DGMX_FFT_LIBRARY=fftpack -DCMAKE_INSTALL_PREFIX=${GMX_INSTALL} ..
+cmake -DGMX_SIMD=NONE -DGMX_GPU=off -DGMXAPI=OFF -DGMX_INSTALL_LEGACY_API=on -DGMX_FFT_LIBRARY=fftpack -DCMAKE_INSTALL_PREFIX=${GMX_INSTALL} ..
 make
 make install
 
